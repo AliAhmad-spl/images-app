d25bc6a390684e2099636a0e8ed5c347b9c9b742
Ali Ahmad <ali.ahmad@eritheialabs.com>
2022-03-11 23:12:51 -0800

    initial commit

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,19 @@
+class ImagesController < ApplicationController
+  def index
+    @images = Image.all
+  end
+
+  def new
+    @image = Image.new
+  end
+
+  def create
+    Image.create(permitted_params)
+    redirect_to images_path
+  end
+
+  private 
+  def permitted_params
+    params.require(:image, :name).permit!
+  end
+end
